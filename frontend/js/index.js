5d76afda8226093178912a46d0e92c8323d1e8cb
vverac <viverace@gmail.com>
2022-07-17 17:29:29 -0400

    third commit: - create formulario.js - create custom-select.js - create custom-select.css - create main.css - add changes to file index.html - add change to file index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,41 +1,60 @@
 const serverUrl = 'http://localhost:3000/'
 const itemsPath = 'productos/'
+const itemsCatPath = 'categorias/'
 
-window.onload = getData()
+
+// window.onload = getData()
 
 const items = document.querySelector('.items')
 
-function getData() {
-  fetch(`${serverUrl}${itemsPath}`)
-    .then((res) => res.json())
-    // .then((data) => console.log(data))
-    .then((data) => printData(data))
-
+const getData = async () => {
+  try {
+    const res = await fetch(`${serverUrl}${itemsCatPath}`)
+    const data = await res.json()
+    printData(data)
+    formularioCliente(data)
+    filtros(data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function printData(data) {
-  const itemContainer = document.createElement('div')
-  itemContainer.className = 'row';
+// const fetchData = async () => {
+//   try {
+//     const res = await fetch(`${serverUrl}${itemsCatPath}`)
+//     const data = await res.json()
+//     printData(data)
+//     filtros(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
+const printData = data => {
+  let productos = ''
   data.forEach(item => {
-    itemContainer.innerHTML += createDomElement(item)
-    items.append(itemContainer)
+    productos += `
+<div class="row ">
+    <div class="col">
+             <div class="item shadow mb-4 pt-3">
+               <h6 class="item-title text-center">${item.name}</h6>
+               <img class="item-image" src=${item.url_image}>
+               <div class="item-details ml-3">
+                 <p>Precio</p>
+                 <h4 class="item-price">${item.price}</h4>
+                 <button class="item-button btn  btn-lg btn-secondary mb-3 addToCart">Añadir al Carrito</button>
+               </div>
+             </div>
+           </div>
+</div>
+    `
+
   })
+  items.innerHTML = productos;
 }
 
-function createDomElement(item) {
-  const itemHtml = `
- <div class="col-12 col-md-3">
-            <div class="item shadow mb-4 pt-3">
-              <h6 class="item-title text-center">ENERGETICA MR BIG</h6>
-              <img class="item-image" src="https://dojiw2m9tvv09.cloudfront.net/11132/product/misterbig3308256.jpg">
-              <div class="item-details ml-3">
-                <p>Precio</p>
-                <h4 class="item-price"> 1999 </h4>
-                <button class="item-button btn btn-secondary mb-3 addToCart">AÑADIR AL CARRITO</button>
-              </div>
-            </div>
-          </div>
-  `
-  return itemHtml;
-}
+
+
+
+getData()
+// fetchData()
